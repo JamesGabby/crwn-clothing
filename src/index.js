46bd7d7affa76bbea08c475a5f7dd48089ed52c8
JamesGabby <jlghfe36@gmail.com>
2021-12-07 13:05:51 +0000

    added styling to sign in component

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import { BrowserRouter, Route, Routes, useParams } from 'react-router-dom';
 import { HomePage } from './pages/homepage/homepage.component';
 import { ShopPage } from './pages/shop/shop.component';
 import { Header } from './components/header/header.component';
+import { SignInAndSignUpPage } from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
 
 export const HatsPage = () =>  (
   <div>
@@ -32,6 +33,7 @@ ReactDOM.render(
       <Route path="/shop" element={<ShopPage />} />
       <Route path="shop/hats" element={<HatsPage />} />
       <Route path="shop/hats/:hatId" element={<Hat />} />
+      <Route path="/signin" element={<SignInAndSignUpPage />} />
     </Routes>
   </BrowserRouter>,
   document.getElementById('root')
