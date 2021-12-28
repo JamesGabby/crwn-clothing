8779098e866e5c58284057d39c9ca2e88558da88
JamesGabby <jlghfe36@gmail.com>
2021-12-28 13:29:32 +0000

    fixed the rendering of the sign in-sign out comps

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import './App.scss';
 import { BrowserRouter, Route, Routes, useParams } from 'react-router-dom';
+import { auth, createuserProfileDocument } from './firebase/firebase.utils';
+import { setCurrentUser } from './redux/user/user.actions';
+import { connect } from 'react-redux';
 import { HomePage } from './pages/homepage/homepage.component';
 import { ShopPage } from './pages/shop/shop.component';
-import { Header } from './components/header/header.component';
+import  Header  from './components/header/header.component';
 import { SignInAndSignUpPage } from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
-import { auth, createuserProfileDocument } from './firebase/firebase.utils';
-import { connect } from 'react-redux';
-import { setCurrentUser } from './redux/user/user.actions';
 
 export const HatsPage = () =>  (
   <div>
@@ -26,23 +26,24 @@ export const Hat = () =>  {
  }
 
 class App extends React.Component {
-
   unsubscribeFromAuth = null;
 
   componentDidMount() {
     const { setCurrentUser } = this.props;
+
     this.unsubscribeFromAuth = auth.onAuthStateChanged(async userAuth => {
       if (userAuth) {
         const userRef = await createuserProfileDocument(userAuth);
+        
         userRef.onSnapshot(snapShot => {
           setCurrentUser({
               id: snapShot.id,
               ...snapShot.data()            
           });
         });
+      } else {
+        setCurrentUser(userAuth);
       }
-      
-      setCurrentUser(userAuth);
     });
   }
 
--- src/index.js
+++ src/index.js
@@ -5,12 +5,10 @@ import App from './App';
 import { Provider } from 'react-redux';
 import store from './redux/store';
 
-
 ReactDOM.render(
   <Provider store={store}>
     <App />
-  </Provider>
-,
+  </Provider>,
   document.getElementById('root')
 );
 
