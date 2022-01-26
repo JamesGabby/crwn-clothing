1d6057bef7c4f73b4b51aaf87ce0241b55fb6361
JamesGabby <jlghfe36@gmail.com>
2022-01-26 12:55:56 +0000

    Updated Shop Data, Update Cart Utils

--- src/redux/store.js
+++ src/redux/store.js
@@ -4,6 +4,6 @@ import rootReducer from './root-reducer';
 
 const middlewares = [logger];
 
-const store = createStore(rootReducer, applyMiddleware(...middlewares));
+const store = createStore(rootReducer, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__());
 
 export default store;
