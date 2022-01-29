8f3434418da3a285e38a193d0e787c8153cf529c
JamesGabby <jlghfe36@gmail.com>
2022-01-29 11:36:04 +0000

    Updated app with Reselect-added memoised selectors

--- src/components/custom-button/custom-button.component.jsx
+++ src/components/custom-button/custom-button.component.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import './custom-button.scss';
 
 export const CustomButton = ({children, isGoogleSignIn, inverted, ...otherProps}) => (
-    <button className={`${inverted ? 'inverted' : ''} custom-button`} {...otherProps}>
+    <button className={`${inverted ? 'inverted' : ''} ${isGoogleSignIn ? 'google-sign-in' : ''} custom-button` }  {...otherProps}>
         {children}
     </button>
 );
