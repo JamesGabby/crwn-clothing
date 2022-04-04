fdf51b37139eeb5ff174214df18e76d6fff4ac51
JamesGabby <jlghfe36@gmail.com>
2022-04-04 14:27:24 +0100

    improved responsiveness

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,8 +1,5 @@
-import { createStore, applyMiddleware } from 'redux';
-import logger from 'redux-logger';
+import { createStore } from 'redux';
 import rootReducer from './root-reducer';
-
-const middlewares = [logger];
 
 const store = createStore(rootReducer, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__());
 
